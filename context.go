13100a9179030aa3b82b9c859b834b3514c017ab
gcode <thienthanh11tclc@gmail.com>
2019-10-24 17:45:03 +0700

    [GAT-24] WIP

--- context.go
+++ context.go
@@ -1,54 +1,24 @@
 package nrcontext
 
 import (
+	"context"
+
 	"bitbucket.org/snapmartinc/newrelic-context/nrgorm"
 	"bitbucket.org/snapmartinc/newrelic-context/nrredis"
-	"context"
-	"fmt"
-	"net/http"
-
 	"github.com/go-redis/redis"
 	"github.com/jinzhu/gorm"
 	"github.com/newrelic/go-agent"
 )
 
-type contextKey int
-
-const txnKey contextKey = 0
-
-// Set NewRelic transaction to context
-func ContextWithTxn(c context.Context, txn newrelic.Transaction) context.Context {
-	return context.WithValue(c, txnKey, txn)
-}
-
-// Get NewRelic transaction from context anywhere
-func GetTnxFromContext(c context.Context) newrelic.Transaction {
-	if tnx := c.Value(txnKey); tnx != nil {
-		fmt.Println("transaction is not nill")
-		return tnx.(newrelic.Transaction)
-	}
-	return nil
-}
-
 // Sets transaction from Context to gorm settings, returns cloned DB
 func SetTxnToGorm(ctx context.Context, db *gorm.DB) *gorm.DB {
-	txn := GetTnxFromContext(ctx)
+	txn := newrelic.FromContext(ctx)
 	return nrgorm.SetTxnToGorm(txn, db)
 }
 
-func SetTnxToGormMiddleware(db *gorm.DB) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
-			txn := GetTnxFromContext(ctx)
-			db = nrgorm.SetTxnToGorm(txn, db)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
-}
 
 // Gets transaction from Context and applies RedisWrapper, returns cloned client
 func WrapRedisClient(ctx context.Context, c *redis.Client) *redis.Client {
-	txn := GetTnxFromContext(ctx)
+	txn := newrelic.FromContext(ctx)
 	return nrredis.WrapRedisClient(txn, c)
 }
--- middleware.go
+++ middleware.go
@@ -50,16 +50,6 @@ func (nr *NewRelicMiddleware) SetTxnNameFunc(fn TxnNameFunc) {
 	nr.nameFunc = fn
 }
 
-// Creates wrapper for your handler
-func (nr *NewRelicMiddleware) Handler(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		txn := nr.app.StartTransaction(nr.nameFunc(r), w, r)
-		r = r.WithContext(ContextWithTxn(r.Context(), txn))
-		defer txn.End()
-		h.ServeHTTP(txn, r)
-	})
-}
-
 func makeTransactionName(r *http.Request) string {
 	return fmt.Sprintf("%s %s", r.Method, r.URL.Path)
 }
